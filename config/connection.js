593cf593a92b8b55d642c8fef1177580cd799f32
Wyatt Sommer <wyatt@hampton.digital>
2018-05-31 16:52:29 -0600

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgersDB"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgersDB"
+    });
+}
 
 // Make connection.
 connection.connect(function (err) {
